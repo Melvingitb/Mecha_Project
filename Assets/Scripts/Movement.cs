c21f682b1b57f2ede7b1926f374c844565f7b01c
Melvin <melvingit@gmail.com>
2023-06-26 01:25:50 -0700

    add camera and move input with drag

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -14,16 +14,63 @@ namespace Player
 {
     public class Movement : MonoBehaviour
     {
+        [Header("Movement")]
+        public float moveSpeed;
+
+        public float groundDrag;
+        
+        [Header("Ground Check")]
+        public float playerHeight;
+        public LayerMask whatIsGround;
+        bool grounded;
+
+        public Transform orientation;
+
+        float horizontalInput;
+        float verticalInput;
+
+        Vector3 moveDirection;
+
+        Rigidbody rb;
+
         // Start is called before the first frame update
         void Start()
         {
-            
+            rb = GetComponent<Rigidbody>();
+            rb.freezeRotation = true;
+        }
+
+        private void FixedUpdate(){
+            MovePlayer();
+        }
+
+        private void MyInput(){
+            horizontalInput = PlayerInputManager.Instance.movement.x;
+            verticalInput = PlayerInputManager.Instance.movement.y;
+        }
+
+        private void MovePlayer(){
+            // calculate movement direction
+            moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
         }
 
         // Update is called once per frame
         void Update()
         {
-            
+            // ground check
+            grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
+            MyInput();
+
+            //handle drag
+            if (grounded){
+                rb.drag = groundDrag;
+            }
+            else{
+                rb.drag = 0;
+            }
         }
 
     }
--- Assets/Scripts/PlayerCam.cs
+++ Assets/Scripts/PlayerCam.cs
@@ -1,37 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using UnityEngine.InputSystem;
 
-public class PlayerCam : MonoBehaviour
-{
-    public float sensX;
-    public float sensY;
-
-    public Transform orientation;
-
-    float xRotation;
-    float yRotation;
-    // Start is called before the first frame update
-    void Start()
+namespace Player{
+    public class PlayerCam : MonoBehaviour
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
+        public float sensX;
+        public float sensY;
 
-    // Update is called once per frame
-    void Update()
-    {
-        //get mouse input
-        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+        public Transform orientation;
+        //public Transform cameraPosition;
+        //public InputAction cameraControl;
 
-        yRotation += mouseX;
+        float xRotation;
+        float yRotation;
+        // Start is called before the first frame update
+        void Start()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
 
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        // Update is called once per frame
+        void Update()
+        {
+            //get mouse input
+            float mouseX = PlayerInputManager.Instance.rotation.x * Time.deltaTime * sensX;
+            float mouseY = PlayerInputManager.Instance.rotation.y * Time.deltaTime * sensY;
 
-        //rotate cam and orientation
-        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
-        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+            yRotation += mouseX;
+
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+            //rotate cam and orientation
+            transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+            orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+            //transform.position = cameraPosition.position;
+        }
     }
 }
