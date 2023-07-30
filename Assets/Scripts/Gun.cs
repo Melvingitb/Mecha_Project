abe851662e397d29e2affbb104e8da91a3f3f2e8
Melvin <melvingit@gmail.com>
2023-07-30 14:18:42 -0700

    finish shoot tutorial

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -5,14 +5,20 @@ namespace Player{
     {
         public float damage = 10f;
         public float range = 100f;
+        public float fireRate = 15f;
+        public float impactForce = 30f;
 
         public Camera fpsCam;
         public ParticleSystem muzzleFlash;
+        public GameObject impactEffect;
+
+        private float nextTimeToFire = 0f;
 
         // Update is called once per frame
         void Update()
         {
-            if (PlayerInputManager.Instance.fire){
+            if (PlayerInputManager.Instance.fire && Time.time >= nextTimeToFire){
+                nextTimeToFire = Time.time + 1f / fireRate;
                 Shoot();
             }
         }
@@ -28,6 +34,13 @@ namespace Player{
                 if (target != null){
                     target.TakeDamage(damage);
                 }
+
+                if (hit.rigidbody != null){
+                    hit.rigidbody.AddForce(-hit.normal * impactForce);
+                }
+
+                GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+                Destroy(impactGO, 2f);
             }
         }
     }
